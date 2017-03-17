2f0dae98a4cddb390ef70a5c79f39257132c5f1f
Oshevchuk <megadevice83@gmail.com>
2017-03-17 11:47:27 +0200

    profile and home pages

---
+++ frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Oshevchuk on 17.03.2017.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId:module.id,
+    selector: 'home',
+    templateUrl: 'home.component.html',
+})
+export class HomeComponent  { name = 'Angular'; }
---
+++ frontend/src/app/components/profile/profile.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'profile',
+    template: `<h1>wHello 21w1{{name}}</h1>`,
+})
+export class ProfileComponent  { name = 'Angular'; }
